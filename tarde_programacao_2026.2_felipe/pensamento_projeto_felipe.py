963b75348f3f34b311c249f2777176c44510526d
Aluno em Vocace felipe oliveira <araolifelipe123@gmail.com>
2026-06-24 15:27:24 -0300

    tipo_descricao_do_commit

---
+++ tarde_programacao_2026.2_felipe/pensamento_projeto_felipe.py
@@ -0,0 +1,161 @@
+'''
+um bloco de comentarios
+
+projeto barbearia
+PO (Como dono do projeto: quero um sistema para agendar serviços para a barbearia)
+
+>QA (como cliente: quero um sistema para agendar serviços para a barbearia
+para que eu possa agendar um horario com mais facilidade)
+
+>tech (como programador: quero um sistema para agendar serviços para a barbearia,
+ para  que eu possa desenvolver um software eficiente e funcional)
+
+>dev (como programador quero um sistema para agendar serviços para a barbearia,
+para que eu possas implementar as funcionalidades necessárias para meus clientes)
+
+ >UX (como designer de experiência do usuário: quero um sistema para agendar serviços
+para minha barbearia, para que eu possa criar uma interface intuitiva, confortável e 
+agradável para os usuários, garantindo uma experiência de agendamento fácil.)
+
+>IA (Como analista de dados: Quero um sistema de agendamento para minha barbearia,
+ para que possa coletar e analisar os dados de agendamneto, ajudando a indentificar padroes de estilos e otimizar
+ as estrategias de marketing )
+
+
+'''
+
+
+
+
+
+# isso e um comentario de linha unica.
+print ("ola, mundo!")
+
+print ("-" * 48 + "\n")
+print ("bem-vindo ao sistema de agendamento da barbearia\n"
+    )
+print ("1 - agendar corte")
+print ("2 - ver outros cortes")
+print ("3 - nossa localização")
+print ("4 - nosso contato")
+print ("5 - sobre nos")
+print ("6 - funconamento")
+print ("7 - creditos")
+print ("0 - sair do sistema\n")
+print ("\n----------------------------------------------\n")
+
+opção_definida = int(input("digite a opção desejada: "))
+
+if (
+
+)'opcao_definida == cadastrar produto'
+
+if opção_definida == 1 ;
+
+    print("opção 1 - agendar corte...")
+
+elif opção_definida == 2:
+    print ("opção 2 - ver outros cortes")
+
+elif opção_definida == 3:
+    print ("opção 3 nossa localização")
+
+elif opção_definida == 4:
+    print ("opção 4 - nosso contato")
+
+elif opção_definida == 5:
+    print ("opção 5 - sobre nos")
+
+elif opção_definida == 6:
+    print ("opção 6 - funconamento")
+
+elif opção_definida == 7:
+    print ("opção 7 - creditos")
+
+elif opção_definida == 0:
+    print ("opção 0 sair do sistema")
+
+else:
+    print('\n---------------------------------\n')
+
+if opcao == '1':
+    print('Cadastrando produtos...\n')
+
+    # faça a lógica para cadastrar o produto aqui,
+    # e somente a inserção dos dados usando input e os tipos de dados.
+
+    if p1_nome == '':
+        p1_nome = input('corte ')
+        
+        
+        
+        if p1_nome == '':
+        p1_nome = input('Corte: ')
+        p1_estoque = int(input('victor, kauan e felipe: '))
+        p1_preco = float(input('30: '))
+        p1_validade = input('7 dias: ')
+        p1_descricao = input('o melhor corte da regiao: ')
+        print(f'\n📦 Produto "{p1_nome}" cadastrado na vaga 1!') 
+        
+        
+        
+    
+                    
+        
+        
+    
+    
+    
+    elif p2_nome == '':
+        p2_nome = input('barba ')
+        p2_estoque = int(input('victor,Kauan,Felipe,Gabriel,Gustavo '))
+        p2_preco = float(input('20,00 '))
+        p2_validade = input('7 dias ')
+        p2_descricao = input('a melhor barbearia Da Regiao ')
+        print(f'\n📦 Produto "{p2_nome}" cadastrado na vaga 2!')
+
+    elif p3_nome == '':
+        p3_nome = input('sombrancelha ')
+        p3_estoque = int(input('Victor,Kauan,Felipe,Gabriel,Gustavo '))
+        p3_preco = float(input('7,00 '))
+        p3_validade = input('7 dias ')
+        p3_descricao = input('O melhor Estilo ')
+        print(f'\n📦 Produto "{p3_nome}" cadastrado na vaga 3!')
+
+    else:
+        print('❌ Sistema cheio! Limite de 3 produtos atingido.')
+
+
+        elif p4_nome == '':
+        p3_nome = input('luzes')
+        p3_estoque = int(input('Victor,Kauan,Felipe,Gabriel,Gustavo '))
+        p3_preco = float(input('50,00 '))
+        p3_validade = input('1 Mes ')
+        p3_descricao = input('Melhor estilo ')
+        print(f'\n📦 Produto "{p3_nome}" cadastrado na vaga 3!')
+
+
+        lif p5_nome == '':
+        p3_nome = input('Alisamento ')
+        p3_estoque = int(input('Victor,Kauan,Felipe,Gabriel,Gustavo '))
+        p3_preco = float(input('45,00 '))
+        p3_validade = input('1 Mes ')
+        p3_descricao = input('O melhor Estilo ')
+        print(f'\n📦 Produto "{p3_nome}" cadastrado na vaga 3!')
+
+        else:
+        print('❌ Sistema cheio! Limite de 3 produtos atingido.')
+    
+
+    elif opcao ==  '3':
+        print ('realizando venda...')
+    if p1_nome == "" and p2_nome == "" and p3_nome == "":
+        print(f''nao ha produtos cadastrados para realizar vendas.
+     )
+    else:
+         nome_venda = input(''digite o nome do produto que deseja vender: '')
+    
+    # testamos o nome digitado contra o produto 1
+     if nome_venda.lower() == p1_nome.lower() and p1_nome != "":
+         qtd_venda = int (input(f''quantas unidades de '' p1_nome  != "":
+          if qtd_venda <=
